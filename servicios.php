73ce10826069d76079043938af544ac269e843a9
RuRo <rurorita@gmail.com>
2021-05-18 12:23:11 +0200

    entrar

--- servicios.php
+++ servicios.php
@@ -6,7 +6,7 @@
 	<!-- Necesidades Básicas de la Página
 	================================================== -->
 	<meta charset="utf-8">
-	<title>ChuguisApp | Inicio</title>
+	<title>ChuguisApp | Servicios</title>
 	<meta name="description" content="ChuguisApp--Monitorización y seguimiento de bebés">
 	<meta name="author" content="http://amggdeveloper.pythonanywhere.com/">
 
@@ -82,7 +82,7 @@
 		<!-- Main -->
 		<div class="main" role="main">
 
-			<!-- Page Heading -->
+			<!-- Empieza Breadcrumbs -->
 			<section class="page-heading">
 				<div class="container">
 					<div class="row">
@@ -98,142 +98,135 @@
 					</div>
 				</div>
 			</section>
-			<!-- Page Heading / End -->
+			<!-- Termina Breadcrumbs-->
 
-			<!-- Page Content -->
+			<!-- Empieza Contenidos de la Página -->
 			<section class="page-content">
 				<div class="container">
 
 					<div class="row">
 						<div class="col-md-4">
-							<!-- Icon Box -->
+							<!-- Empieza Caja informativa con iconos -->
 							<div class="icon-box circled icon-box-color__primary">
 								<div class="icon">
 									<i class="fa fa-shield fa-3x"></i>
 								</div>
 								<div class="icon-box-body">
-									<h3>Temporary Childcare</h3>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet sodales lacinia consectetur.
+									<h3>Calendario de Vacunación</h3>
+									Calendario con todas las vacunas que actualmente están recomendadas y obligadas por la mayoría de los pediatras.
 								</div>
-							</div>
-							<!-- Icon Box -->
+							</div>							
 						</div>
-						<div class="col-md-4">
-							<!-- Icon Box -->
+
+						<div class="col-md-4">							
 							<div class="icon-box circled icon-box-color__secondary">
 								<div class="icon">
 									<i class="fa fa-stethoscope fa-3x"></i>
 								</div>
 								<div class="icon-box-body">
-									<h3>Areas Served</h3>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet sodales lacinia consectetur.
+									<h3>Calendario citas médicas</h3>
+									Calendario con todas la citas médicas en un solo lugar para poder verlas de un vistazo.
 								</div>
-							</div>
-							<!-- Icon Box -->
+							</div>							
 						</div>
-						<div class="col-md-4">
-							<!-- Icon Box -->
+
+						<div class="col-md-4">							
 							<div class="icon-box circled icon-box-color__tertiary">
 								<div class="icon">
 									<i class="fa fa-location-arrow fa-3x"></i>
 								</div>
 								<div class="icon-box-body">
-									<h3>Nanny Placement</h3>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet sodales lacinia consectetur.
+									<h3>Monitorización</h3>
+									Podrás monitorizar todos los aspectos de tu bebé en un único lugar, de manera organizada y clara.
 								</div>
-							</div>
-							<!-- Icon Box -->
+							</div>							
 						</div>
 					</div>
 
 					<div class="spacer"></div>
 
 					<div class="row">
-						<div class="col-md-4">
-							<!-- Icon Box -->
+						<div class="col-md-4">						
 							<div class="icon-box circled icon-box-color__quaternary">
 								<div class="icon">
 									<i class="fa fa-file-text-o fa-3x"></i>
 								</div>
 								<div class="icon-box-body">
-									<h3>Mom-Reviewed Nannies</h3>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet sodales lacinia consectetur.
+									<h3>Datos Unificados</h3>
+									Tendremos la posibilidad de tener todos los datos de nuestro bebé unificado, viendo toda la información en un único lugar.
 								</div>
-							</div>
-							<!-- Icon Box -->
+							</div>						
 						</div>
-						<div class="col-md-4">
-							<!-- Icon Box -->
+
+						<div class="col-md-4">							
 							<div class="icon-box circled icon-box-color__primary">
 								<div class="icon">
 									<i class="fa fa-check-circle fa-3x"></i>
 								</div>
 								<div class="icon-box-body">
-									<h3>Background Checks</h3>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet sodales lacinia consectetur.
+									<h3>Seguimiento de Tareas</h3>
+									Ofrecemos un lugar en donde llevar un seguimiento pormenorizado de la vida diaria de nuestro bebé, por ejemplo, la información de la última toma que le hemos dado.
 								</div>
-							</div>
-							<!-- Icon Box -->
+							</div>							
 						</div>
-						<div class="col-md-4">
-							<!-- Icon Box -->
+
+						<div class="col-md-4">							
 							<div class="icon-box circled icon-box-color__senary">
 								<div class="icon">
 									<i class="fa fa-lightbulb-o fa-3x"></i>
 								</div>
 								<div class="icon-box-body">
-									<h3>References</h3>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet sodales lacinia consectetur.
+									<h3>Consejos</h3>
+									Ofrecemos unas guías de productos testeados por nuestro equipo, en el cual se informa de los distintos productos que se pueden utilizar con nuestro bebé.
 								</div>
-							</div>
-							<!-- Icon Box -->
+							</div>						
 						</div>
 					</div>
-
+					<!--Termina Caja informativa con iconos -->
 					<hr class="hr__bold lg">
-
-					<h2>What Clients Say</h2>
+					<!--Empieza Caja Opiniones-->
+					<h2>¿Qué Dicen Nuestros Clientes?</h2>
 					<div class="row">
 						<div class="col-md-4">
 							<div class="testimonial">
 								<blockquote>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut laoreet mi. Integer vitae elit quis leo tincidunt euismod. Nullam blandit vestibulum lectus sed sollicitudin. </p>
+									<p>Aplicación que me ha ayudado a tener en un único sitio todo lo relacionado con mi bebé. Actualmente lleve la monitorización y el seguimiento, y es muy útil. Recomendada totalmente. </p>
 								</blockquote>
 								<div class="bq-author">
 									<figure class="author-img">
-										<img src="http://placehold.it/60x60" alt="">
+										<img src="images/samples/cliente1.jpg" alt="cliente uno">
 									</figure>
-									<h6>Elizabeth McDoe</h6>
-									<span class="bq-author-info">Proin gravida nibh vel</span>
+									<h6>Isabel Martínez</h6>
+									<span class="bq-author-info">Usuaria desde hace 5 meses.</span>
 								</div>
 							</div>
 						</div>
 						<div class="col-md-4">
 							<div class="testimonial">
 								<blockquote>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut laoreet mi. Integer vitae elit quis leo tincidunt euismod. Nullam blandit vestibulum lectus sed sollicitudin. </p>
+									<p>Si quieres un sitio en donde tener todo centralizado, ChuguisApp es la aplicación que necesitas. Antes de utilizarla utilizaba un diario para poner todo lo referente a mi bebé, esto es más comodo. </p>
 								</blockquote>
 								<div class="bq-author">
 									<figure class="author-img">
-										<img src="http://placehold.it/60x60" alt="">
+										<img src="images/samples/cliente2.jpg" alt="cliente dos">
 									</figure>
-									<h6>Megan Doe</h6>
-									<span class="bq-author-info">Proin gravida nibh vel</span>
+									<h6>Anabel García</h6>
+									<span class="bq-author-info">Usuaria desde hace 3 meses.</span>
 								</div>
 							</div>
 						</div>
 						<div class="col-md-4">
 							<div class="testimonial">
 								<blockquote>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut laoreet mi. Integer vitae elit quis leo tincidunt euismod. Nullam blandit vestibulum lectus sed sollicitudin. </p>
+									<p>Con esta aplicación puedes saber si tu hij@ está cogiendo peso y entra dentro de los percentiles normales. Sólo con un vistazo puedo ver toda esa información, la recomiendo 100%. </p>
 								</blockquote>
 								<div class="bq-author">
 									<figure class="author-img">
-										<img src="http://placehold.it/60x60" alt="">
+										<img src="images/samples/cliente3.jpg" alt="cliente tres">
 
 									</figure>
-									<h6>Fransisca Hall</h6>
-									<span class="bq-author-info">Proin gravida nibh vel</span>
+									<h6>Paco Remiro</h6>
+									<span class="bq-author-info">Usuario desde hace 7 meses.</span>
 								</div>
 							</div>
 						</div>
@@ -245,53 +238,54 @@
 						<div class="col-md-4">
 							<div class="testimonial">
 								<blockquote>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut laoreet mi. Integer vitae elit quis leo tincidunt euismod. Nullam blandit vestibulum lectus sed sollicitudin. </p>
+									<p>Aplicación muy muy comoda para tener todo a mano en relación a información de tomas y alimentos que le hemos introducido a nuestra hija. Fácil de usar. </p>
 								</blockquote>
 								<div class="bq-author">
 									<figure class="author-img">
-										<img src="http://placehold.it/60x60" alt="">
+										<img src="images/samples/cliente4.jpg" alt="cliente cuatro">
 
 									</figure>
-									<h6>Helen Taylor</h6>
-									<span class="bq-author-info">Proin gravida nibh vel</span>
+									<h6>Hugo González</h6>
+									<span class="bq-author-info">Usuario desde hace 2 meses.</span>
 								</div>
 							</div>
 						</div>
 						<div class="col-md-4">
 							<div class="testimonial">
 								<blockquote>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut laoreet mi. Integer vitae elit quis leo tincidunt euismod. Nullam blandit vestibulum lectus sed sollicitudin. </p>
+									<p>Ahora en la familia podemos controlar los alimentos que hemos introducido a mi hermano pequeño. La he recomendado a mis amig@s. </p>
 								</blockquote>
 								<div class="bq-author">
 									<figure class="author-img">
-										<img src="http://placehold.it/60x60" alt="">
+										<img src="images/samples/cliente5.jpg" alt="cliente cinco">
 
 									</figure>
-									<h6>Mika Grey</h6>
-									<span class="bq-author-info">Proin gravida nibh vel</span>
+									<h6>Luisa Ruíz</h6>
+									<span class="bq-author-info">Usuaria desde hace 9 meses</span>
 								</div>
 							</div>
 						</div>
 						<div class="col-md-4">
 							<div class="testimonial">
 								<blockquote>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut laoreet mi. Integer vitae elit quis leo tincidunt euismod. Nullam blandit vestibulum lectus sed sollicitudin. </p>
+									<p>He probado muchas aplicaciones de este tipo, pero por ahora esta es muy simple y sencilla. Hay aspectos mejorables, pero se nota que están en constante cambio para mejorar. La recomiendo. </p>
 								</blockquote>
 								<div class="bq-author">
 									<figure class="author-img">
-										<img src="http://placehold.it/60x60" alt="">
+										<img src="images/samples/cliente6.jpg" alt="cliente seis">
 
 									</figure>
-									<h6>Stefanny Bailey</h6>
-									<span class="bq-author-info">Proin gravida nibh vel</span>
+									<h6>D'Artacán</h6>
+									<span class="bq-author-info">Usuario desde hace 11 meses.</span>
 								</div>
 							</div>
 						</div>
 					</div>
 
 					<hr class="hr__bold lg">
-
-					<h2>Our Clients</h2>
+					<!--Termina Caja Opiniones-->
+					<!--Empieza Marcas Testadas-->
+					<h2>Marcas Testadas</h2>
 					<div class="row">
 						<div class="row">
 							<div class="col-sm-3 col-md-3">
@@ -314,35 +308,11 @@
 									<a href="#"><img src="images/samples/client-logo1.png" alt=""></a>
 								</div>
 							</div>
-						</div>
-						<div class="spacer"></div>
-						<div class="row">
-							<div class="col-sm-3 col-md-3">
-								<div class="hovered-img text-center">
-									<a href="#"><img src="images/samples/client-logo1.png" alt=""></a>
-								</div>
-							</div>
-							<div class="col-sm-3 col-md-3">
-								<div class="hovered-img text-center">
-									<a href="#"><img src="images/samples/client-logo4.png" alt=""></a>
-								</div>
-							</div>
-							<div class="col-sm-3 col-md-3">
-								<div class="hovered-img text-center">
-									<a href="#"><img src="images/samples/client-logo2.png" alt=""></a>
-								</div>
-							</div>
-							<div class="col-sm-3 col-md-3">
-								<div class="hovered-img text-center">
-									<a href="#"><img src="images/samples/client-logo3.png" alt=""></a>
-								</div>
-							</div>
-						</div>
-					</div>
-
+						</div>					
 				</div>
+				<!--Termina Marcas Testadas-->
 			</section>
-			<!-- Page Content / End -->
+			<!-- Termian Contenidos de la Página -->
 
 			<!-- Empieza Footer -->
 			<?php include 'includes/footer.php';?>
